6ce1b1f438fdd2ee26d52492059e41d714c8512a
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2022-12-28 10:05:47 -0600

    Return json.UnmarshalTypeError when date unmarshalling fails
    Fixes #122

---
+++ beans/date_test.go
@@ -0,0 +1,28 @@
+package beans
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDateUnmarshal(t *testing.T) {
+
+	t.Run("can unmarshal valid date", func(t *testing.T) {
+		var d Date
+		err := json.Unmarshal([]byte(`"2022-05-11"`), &d)
+		require.Nil(t, err)
+		assert.Equal(t, "2022-05-11", d.String())
+	})
+
+	t.Run("unmarshal invalid date returns unmarshal error", func(t *testing.T) {
+		var d Date
+		err := json.Unmarshal([]byte(`"2022-0511"`), &d)
+		require.NotNil(t, err)
+		var jsonErr *json.UnmarshalTypeError
+		require.ErrorAs(t, err, &jsonErr)
+		assert.Equal(t, `"2022-0511"`, jsonErr.Value)
+	})
+}
