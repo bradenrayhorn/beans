dd9a59e1484bdc02c59ace1ef99582b02af56e36
Braden Rayhorn <25675893+bradenrayhorn@users.noreply.github.com>
2024-02-23 21:55:53 -0600

    Make specification tests run in parallel

--- server/specification/test.go
+++ server/specification/test.go
@@ -4,11 +4,32 @@ import "testing"
 
 func DoTests(t *testing.T, interactor Interactor) {
 
-	t.Run("account", func(t *testing.T) { testAccount(t, interactor) })
-	t.Run("budget", func(t *testing.T) { testBudget(t, interactor) })
-	t.Run("category", func(t *testing.T) { testCategory(t, interactor) })
-	t.Run("month", func(t *testing.T) { testMonth(t, interactor) })
-	t.Run("payee", func(t *testing.T) { testPayee(t, interactor) })
-	t.Run("transaction", func(t *testing.T) { testTransaction(t, interactor) })
-	t.Run("user", func(t *testing.T) { testUser(t, interactor) })
+	t.Run("account", func(t *testing.T) {
+		t.Parallel()
+		testAccount(t, interactor)
+	})
+	t.Run("budget", func(t *testing.T) {
+		t.Parallel()
+		testBudget(t, interactor)
+	})
+	t.Run("category", func(t *testing.T) {
+		t.Parallel()
+		testCategory(t, interactor)
+	})
+	t.Run("month", func(t *testing.T) {
+		t.Parallel()
+		testMonth(t, interactor)
+	})
+	t.Run("payee", func(t *testing.T) {
+		t.Parallel()
+		testPayee(t, interactor)
+	})
+	t.Run("transaction", func(t *testing.T) {
+		t.Parallel()
+		testTransaction(t, interactor)
+	})
+	t.Run("user", func(t *testing.T) {
+		t.Parallel()
+		testUser(t, interactor)
+	})
 }
